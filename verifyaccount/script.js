1fb593dd20085dec7e8b53c6403dfdf4f0b84173
= <kevinpmcdaniel@gmail.com>
2023-09-25 14:48:21 -0700

    added code verification ui project

---
+++ verifyaccount/script.js
@@ -0,0 +1,30 @@
+const codes = document.querySelectorAll('.code');
+
+codes[0].focus();
+
+codes.forEach((code, idx) => {
+    code.addEventListener('keydown', e =>{
+        if(e.key >= 0 && e.key <= 9){
+            codes[idx].value='';
+            console.log(`index: ${idx} max: ${codes.length}`)
+            setTimeout(() => {
+                if(idx === codes.length - 1){
+                    codes[codes.length - 1].focus();
+                } else {
+                    codes[idx + 1].focus();
+                }
+                
+            }, 10 )
+            
+        } else if(e.key === 'Backspace'){
+            setTimeout(() => {
+                if(idx-1 < 0){
+                    codes[0].focuse
+                } else {
+                    codes[idx - 1].focus();
+                }
+                
+            }, 10 )
+        }
+    })
+})
