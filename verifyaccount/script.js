764a77e34605e81c0124f9faecc2d74a68dccb81
= <kevinpmcdaniel@gmail.com>
2023-09-25 14:49:14 -0700

    cleaned up verification ui project

--- verifyaccount/script.js
+++ verifyaccount/script.js
@@ -6,7 +6,7 @@ codes.forEach((code, idx) => {
     code.addEventListener('keydown', e =>{
         if(e.key >= 0 && e.key <= 9){
             codes[idx].value='';
-            console.log(`index: ${idx} max: ${codes.length}`)
+            
             setTimeout(() => {
                 if(idx === codes.length - 1){
                     codes[codes.length - 1].focus();
@@ -19,7 +19,7 @@ codes.forEach((code, idx) => {
         } else if(e.key === 'Backspace'){
             setTimeout(() => {
                 if(idx-1 < 0){
-                    codes[0].focuse
+                    codes[0].focus()
                 } else {
                     codes[idx - 1].focus();
                 }
